92a0a0c4b0f989fd807970528d4c4842b2669119
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-02-19 20:02:19 +0200

    Edit points-on-line.cpp

--- points-on-line.cpp
+++ points-on-line.cpp
@@ -13,19 +13,19 @@ struct hash_pair {
     } 
 };
 
-int maxPointOnSameLine(std::vector<std::pair<int, int>> points) {
+int maxPointsLine(std::vector<std::pair<int, int>> points) {
     int N = points.size();
     if (N < 2)
         return N;
     
     int maxPoint = 0;
-    int curMax, overlapPoints, verticalPoints;
+    int currentMax, overlapingPoints, verticalPoints;
     std::unordered_map<std::pair<int, int>, int, hash_pair> slopeMap;
     for (int i = 0; i < N; i++) {
-        curMax = overlapPoints = verticalPoints = 0;
+        currentMax = overlapingPoints = verticalPoints = 0;
         for (int j = i + 1; j < N; j++) {
             if (points[i] == points[j])
-                overlapPoints++;
+                overlapingPoints++;
             else if (points[i].first == points[j].first)
                 verticalPoints++;
             else {
@@ -35,11 +35,11 @@ int maxPointOnSameLine(std::vector<std::pair<int, int>> points) {
                 deltaY /= g;
                 deltaX /= g;
                 slopeMap[std::make_pair(deltaY, deltaX)]++;
-                curMax = std::max(curMax, slopeMap[std::make_pair(deltaY, deltaX)]); 
+                currentMax = std::max(currentMax, slopeMap[std::make_pair(deltaY, deltaX)]); 
             }
-            curMax = std::max(curMax, verticalPoints);
+            currentMax = std::max(currentMax, verticalPoints);
         }
-        maxPoint = std::max(maxPoint, curMax + overlapPoints + 1);
+        maxPoint = std::max(maxPoint, currentMax + overlapingPoints + 1);
         slopeMap.clear();
     }
     return maxPoint;
@@ -54,6 +54,6 @@ int main() {
     std::vector<std::pair<int, int>> points;
     for (int i = 0; i < n; i++)
         points.push_back(std::make_pair(pointsArr[i][0], pointsArr[i][1]));
-    std::cout << maxPointOnSameLine(points) << std::endl;
+    std::cout << maxPointsLine(points) << std::endl;
     return 0;
 }
