c4e286cc5a908f0face4364980fa048854990fb7
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-02-20 23:30:54 +0200

    Add files

--- 04-typedef.cpp
+++ 04-typedef.cpp
@@ -7,7 +7,9 @@ int main() {
     typedef unsigned int UINT;
     USHORT s;
     UINT i;
-    std::cout << "Type of USHORT: " << typeid(int).name() << std::endl;
+    std::cout << "Type of USHORT: " << typeid(s).name() << std::endl;
+    std::cout << "Type of unsigned short: " << typeid(unsigned short).name() << std::endl;
     std::cout << "Type of UINT: " << typeid(i).name() << std::endl;
+    std::cout << "Type of unsigned int: " << typeid(unsigned int).name() << std::endl;
     return 0;
 }
